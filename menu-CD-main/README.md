8bbec052053d09b10da0049b581a36e735a46e6d
Mango <hoangtung1771@gmail.com>
2024-11-21 12:53:24 +0700

    Add files via upload

---
+++ menu-CD-main/README.md
@@ -0,0 +1,47 @@
+# Menu script nhằm hỗ trợ sử dụng vps thuận tiện hơn
+## Cài đặt
+```
+bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/menu-CD/main/menu-script)
+```
+## Chức năng chính
+- MENU X-UI
+1. Cài đặt X-UI và mở Port cần thiết 
+2. Gỡ cài đặt bảng điều khiển 
+3. Cập nhật X-UI 
+4. Dừng bảng điều khiển 
+5. Khởi động bảng điều khiển 
+6. Khởi động lại bảng điều khiển 
+7. Gỡ cài đặt X-UI
+
+- MENU Speedtest
+1. test và cài speedtest
+2. gỡ cài đặt speedtest
+
+- MENU Tăng Tốc VPS
+0. Nâng cấp tập lệnh
+1. Cài đặt hạt nhân BBR gốc
+2. Cài đặt hạt nhân BBRplus
+3. Cài đặt nhân Lotserver (Ruisu)
+5. Cài đặt hạt nhân BBRplus mới
+6. Cài đặt nhân phiên bản xanmod
+9. Chuyển sang phiên bản hạt nhân mà không cần gỡ cài đặt
+10. Chuyển sang tập lệnh hệ thống DD bằng một cú nhấp chuột
+11. Sử dụng BBR + FQ để tăng tốc 
+12. Sử dụng BBR + FQ_PIE để tăng tốc
+13. Sử dụng BBR + CAKE để tăng tốc
+14. Sử dụng BBR2 + FQ để tăng tốc 
+15. Sử dụng BBR2 + FQ_PIE để tăng tốc
+16. Sử dụng BBR2 + CAKE để tăng tốc
+17. Bật ECN 18. Tắt ECN
+19. Sử dụng phiên bản BBRplus + FQ để tăng tốc
+20. Sử dụng Lotserver (tốc độ nhanh) để tăng tốc
+21. Tối ưu hóa cấu hình hệ thống 
+22. Ứng dụng chương trình tối ưu hóa johnrosen1
+23. Tắt IPv6 24. Bật IPv6
+25. Gỡ cài đặt tất cả tăng tốc 
+
+- đổi mật khẩu vps
+- cập nhật vps
+- cập nhật menu
+- Thông báo
+
---
+++ menu-CD-main/menu.sh
@@ -0,0 +1,287 @@
+#!/usr/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+echo " Make by DauDau "
+
+menu()
+{
+	echo " "                                  
+	echo -e " ${green}==============================================${plain}"
+	echo -e " ${green}===================${plain} Mango ${green}===========[${plain}4.3.2${green}]${plain}"
+	echo -e " ${green}=${plain}.•♫•♬•.•♫•♬•  MENU CÀI ĐẶT VPS  •♫•♬•.•♫•♬•.${green}=${plain}"
+	echo -e " ${green}==============================================${plain}"
+	echo -e " ${green}=${plain} (1). Menu X-Ui                             ${green}=${plain}"
+	echo -e " ${green}=${plain} (2). Menu Speedtest                        ${green}=${plain}"
+	echo -e " ${green}=${plain} (3). Menu tăng tốc VPS                     ${green}=${plain}"
+	echo -e " ${green}=${plain} (4). Đổi mật khẩu VPS                      ${green}=${plain}"
+	echo -e " ${green}=${plain} (5). Cập nhật VPS                          ${green}=${plain}"
+	echo -e " ${green}=${plain} (6). Cập nhật Menu                         ${green}=${plain}"
+	echo -e " ${green}=${plain} (7). Thông Báo mới                         ${green}=${plain}"
+	echo -e " ${green}=${plain} (0). Đóng menu                             ${green}=${plain}"
+	echo -e " ${green}==============================================${plain}"
+	echo -n "  Lựa chọn của bạn là: "
+	read so
+
+	if [ $so -eq 1 ]
+	then
+        clear
+        menu_x-ui
+        
+	elif [ $so -eq 2 ]
+	then
+        clear
+        menu_speedtest
+        
+	elif [ $so -eq 3 ]
+	then
+        clear
+        echo " "
+        echo "  ★★★ Triển Khai Tăng Tốc Cho Bạn ★★★ "
+	echo " "
+        bash <(curl -L -s https://raw.githubusercontent.com/DauDau432/Linux-NetSpeed/master/tcp.sh)
+	clear
+        menu
+        
+	elif [ $so -eq 4 ]
+	then
+        clear
+        echo " "
+        echo "  ★ Mời Bạn Nhập Mật Khẩu Mới Và Xác Nhận Lại Mật Khẩu ★ "
+        echo "  == Mật Khẩu Bảo Mật Nên Không Nhìn Được Đâu. Nhập Đi == "
+        echo " -------------------------------------- "
+        lsb\_release -a
+	DATE=`date`
+	echo " -------------------------------------- "
+        echo "  Date is $DATE "
+        echo " -------------------------------------- "
+        echo "  Bắt Đầu Nhập... "
+        passwd
+        menu
+        
+        elif [ $so -eq 5 ]
+	then
+        clear
+        apt-get update -y
+	apt upgrade -y
+        clear
+        echo " "
+        echo "  ★★★ Đã Cập Nhật VPS ★★★ "
+        menu
+	
+	elif [ $so -eq 6 ]
+	then
+        clear
+        bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/menu-CD/main/menu-script)
+        clear
+        echo " "
+        echo "  ★★★ Đã Cập Nhật Menu ★★★ "
+        menu
+	
+	elif [ $so -eq 7 ]
+	then
+        clear
+	menu_thongbao
+        
+	elif [ $so -eq 0 ]
+	then
+        clear
+        echo " "
+        echo " =========== Ơ Kìa Sao Lại Đóng Menu =========== "
+        echo " "
+        echo "  Nếu Muốn Gọi Menu Hãy Nhập Lệnh:  bash menu.sh  "
+        echo " ------------------------------------------------------- "
+		echo "  Github: https://github.com/Mango-git-dev"
+        echo " ------------------------------------------------------- "
+        exit 
+               
+	else
+        clear
+        echo " "
+        echo " === Số Bạn Chọn Không Có Trong Bảng Điều Khiển === "
+        echo " "
+        echo " ============ Chọn sai ời. chọn lại đi ============ "
+        menu
+	fi
+}
+
+menu_x-ui()
+{
+	echo " "
+	echo " ==============================================="
+	echo " =================== Mango  ===========[4.3.2]"
+	echo " =    .•♫•♬•.•♫•♬•  MENU X-UI  •♫•♬•.•♫•♬•.    ="
+	echo " ==============================================="
+	echo " = (1). Menu quản lý X-UI (nhiều chức năng hơn)="
+	echo " = (2). Cài đặt X-UI và mở Port cần thiết      ="
+	echo " = (3). Gỡ cài đặt bảng điều khiển             ="
+	echo " = (4). Cập nhật X-UI                          ="
+	echo " = (5). Dừng bảng điều khiển                   ="
+	echo " = (6). Khởi động bảng điều khiển              ="
+	echo " = (7). Khởi động lại bảng điều khiển          ="
+	echo " = (8). Gỡ cài đặt X-UI                        ="
+	echo " = (0). Quay lại                               ="
+	echo " ==============================================="
+	echo -n "  Lựa chọn của bạn là: "
+	read so
+	
+	if [ $so -eq 1 ]
+	then
+        clear
+	x-ui
+	clear
+	menu_x-ui
+
+	elif [ $so -eq 2 ]
+	then
+        clear
+	ufw allow 54321
+        ufw allow 443
+        ufw allow 80
+        ufw allow 80/tcp 
+	firewall-cmd --zone=public --add-port=54321/tcp --permanent
+        firewall-cmd --zone=public --add-port=80/tcp --permanent
+        firewall-cmd --zone=public --add-port=443/tcp --permanent
+        firewall-cmd --reload
+	bash <(curl -Ls https://raw.githubusercontent.com/DauDau432/VH_x-ui/main/install.sh)
+	# gọi IP server
+        intenal_ip=$(ip addr | grep -E -o '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | grep -E -v "^127\.|^255\.|^0\." | head -n 1)#copyright by daudau
+	clear
+	echo " "
+        echo -e "  ${green}x-ui v0.3.2${plain} Quá trình cài đặt hoàn tất và bảng điều khiển đã được bật"
+        echo -e ""
+        echo -e "  Hãy đảm bảo cổng ${green}54321${plain} không bị các chương trình khác chiếm Và Cổng ${green}54321${plain} đã được mở"
+        echo -e "  Nếu bạn muốn đổi ${green}54321${plain} thành một cổng khác, hãy nhập lệnh ${green}x-ui${plain} để đổi và đảm bảo cổng đã đó cũng được mở"
+        echo -e ""
+        echo -e "  Vào trình duyệt truy cập  ${green}http://${intenal_ip}:54321${plain}  để vào bảng điều khiển"
+        echo -e "  Tài khoản và mật khẩu mặc định là:  ${green}admin${plain}"
+        menu_x-ui
+        
+	elif [ $so -eq 3 ]
+	then 
+	echo " ---------------------------------------------- "
+	echo "  Bấm Y để đồng ý "
+	echo " ---------------------------------------------- "
+        x-ui uninstall
+        menu_x-ui
+        
+	elif [ $so -eq 4 ]
+	then
+	echo " ---------------------------------------------- "
+	echo "  Bấm Y để đồng ý "
+	echo " ---------------------------------------------- "
+        x-ui update
+        menu_x-ui
+        
+	elif [ $so -eq 5 ]
+	then
+        x-ui stop 
+        menu_x-ui  
+        
+	elif [ $so -eq 6 ]
+	then
+        x-ui start  
+        menu_x-ui   
+
+	elif [ $so -eq 7 ]
+	then
+        x-ui restart 
+        menu_x-ui   
+        
+	elif [ $so -eq 8 ]
+	then
+        rm /usr/bin/x-ui
+        menu_x-ui 
+	
+	elif [ $so -eq 0 ]
+	then
+        clear
+        menu
+              
+	else
+        clear
+        echo " "
+        echo " === Số Bạn Chọn Không Có Trong Bảng Điều Khiển === "
+        echo " "
+        echo " ============ Chọn sai ời. chọn lại đi ============ "
+        menu_x-ui
+	fi
+}
+
+menu_speedtest()
+{
+	echo " "
+	echo " =============================================="
+	echo " =================== Mango =============[4.3.2]"
+	echo " = .•♫•♬•.•♫•♬•  MENU SPEEDTEST  •♫•♬•.•♫•♬•. ="
+	echo " =============================================="
+	echo " = (1). Cài đặt Speedtest                     ="
+	echo " = (2). Gỡ cài đặt Speedtest                  ="
+	echo " = (0). Quay lại                              ="
+	echo " =============================================="
+	echo -n "  Lựa chọn của bạn là: "
+	read so
+
+	if [ $so -eq 1 ]
+	then
+        clear
+	apt install speedtest-cli
+	clear
+	echo " "
+	echo "  ★★★ Mạng Chậm Như Rùa Cũng Test :)) ★★★ "
+	echo " "
+	speedtest-cli --simple --bytes
+	DATE=`date`
+	echo " ---------------------------------------------- "
+        echo "  Date is $DATE "
+        menu_speedtest
+        
+	elif [ $so -eq 2 ]
+	then
+	echo " ---------------------------------------------- "
+	echo "  Bấm Y để đồng ý "
+	echo " ---------------------------------------------- "
+        apt remove speedtest-cli
+        clear
+        menu_speedtest
+	
+	elif [ $so -eq 0 ]
+	then
+        clear
+        menu
+              
+	else
+        clear
+        echo " "
+        echo " === Số Bạn Chọn Không Có Trong Bảng Điều Khiển === "
+        echo " "
+        echo " ============ Chọn sai ời. chọn lại đi ============ "
+        menu_speedtest
+	fi
+}
+
+menu_thongbao()
+{        
+        clear
+        echo " "
+	echo "  ★★★★★★★★★★★★ Thông Báo ★★★★★★★★★★★★ "
+	DATE=`date`
+	echo " "
+        echo "  Date is $DATE "
+        echo " ------------------------------------------------------------------------ "
+	echo "  Hiện tại không có thông báo nào mới "
+	echo "  nếu có góp ý nào hoặc phát hiện lỗi vui lòng liên hệ với mình để mình khắc phục sớm"
+		echo "  Github: https://github.com/Mango-git-dev "
+        echo " ------------------------------------------------------------------------ "
+	echo "  Bấm phím Enter để quay lại... "
+	read so
+	if [ $so=true ]
+	then 
+	clear 
+        menu
+	fi 
+}	
+menu
